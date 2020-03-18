cdf6563de9fe2cea8fddf1855193dbe147731392
student <bobberino98@gmail.com>
2020-03-17 20:00:30 -0700

    fixed errors in database code

--- Dev/PostAPI/model.py
+++ Dev/PostAPI/model.py
@@ -3,7 +3,7 @@ from flask import jsonify
 class Schema:
     def __init__(self):
         # connecting to the database
-        self.conn = sqlite3.connect('posts.db')
+        self.conn = sqlite3.connect('posts.db', isolation_level=None)
         # table 
         self.create_posts_table()
 
@@ -25,16 +25,18 @@ class Schema:
 
 class PostModel:
     def __init__(self):
-        self.conn = sqlite3.connect('posts.db')
+        self.conn = sqlite3.connect('posts.db', isolation_level=None)
         self.table_name = 'Post'
 
     def create_post(self, title, text, community, URL, username, postDate):
         
         query = f'insert into {self.table_name} ' \
                 f'(title, text, community, URL, username, postDate) ' \
-                f'values ("{title}","{text}", "{community}", "{URL}", "{username}", "{postDate}")'
+                f'values ("{title}","{text}", "{community}", "{URL}", "{username}", "{postDate}");'
+        
         try:
-            self.conn.execute(query)
+            self.conn.execute(query);
+
             return {'message': f'Post with the title:{title} is created!'}
             self.conn.commit()
         except:
@@ -42,7 +44,7 @@ class PostModel:
     
     def delete_post(self, title):
 
-        query = f'DELETE FROM {self.table_name} WHERE title = "{title}"'
+        query = f'DELETE FROM {self.table_name} WHERE title = "{title}";'
         try:
             self.conn.execute(query)
             return {'message': f'Post with the title:{title} is deleted!'}
@@ -51,18 +53,20 @@ class PostModel:
             return {'message': 'Could not delete the post'}
     
     def retrieve_post(self, title):
-        query = f'SELECT * FROM {self.table_name} WHERE title = "{title}"'
+        query = f'SELECT * FROM {self.table_name} WHERE title = "{title}";'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
     
     def list_particular(self, community, n):
         query = f'SELECT * FROM {self.table_name} WHERE community = "{community}"' \
-                f'ORDER BY postDate DESC LIMIT {n}'
+                f'ORDER BY postDate DESC LIMIT {n};'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
     
     def list_recent(self, n):
         query = f'SELECT * FROM {self.table_name} ' \
-                f'ORDER BY postDate'
+                f'ORDER BY postDate;'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
+    def close(self):
+        self.conn.close()
